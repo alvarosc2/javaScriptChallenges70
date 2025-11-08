228bff2907e510b27812850656cb602801907791
alvarosc2 <alvarosc2@gmail.com>
2025-11-08 15:55:00 -0600

    Permutations  recursive. Console log removal.

--- permutations-recursive/permutations.ts
+++ permutations-recursive/permutations.ts
@@ -8,9 +8,8 @@ function permutations(str: string): string[] {
 
     for (let i = 0; i < str.length; i++) {
         const firstChar = str[i];
-        console.log('First char id: ' + firstChar);
+
         const restOfString = str.slice(0, i) + str.slice(i + 1);
-        console.log('rest of string: ' + restOfString);
         const subpermutations = permutations(restOfString);
 
         for (let j = 0; j < subpermutations.length; j++) {
