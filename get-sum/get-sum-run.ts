0d9f1aa994e377ef3868ef96338facd522a12bb6
alvarosc2 <alvarosc2@gmail.com>
2025-11-09 23:08:52 -0600

    get sum of two numbers

---
+++ get-sum/get-sum-run.ts
@@ -0,0 +1,6 @@
+//@ts-check
+
+import { getSum } from './get-sum';
+
+console.log(getSum(3, 5));
+console.log(getSum(10, 8));
---
+++ get-sum/get-sum.ts
@@ -0,0 +1,5 @@
+function getSum(number1: number, number2: number): number {
+    return number1 + number2;
+}
+
+export { getSum }
