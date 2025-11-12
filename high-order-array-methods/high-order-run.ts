6fd70863a8d229073432d95ea4ae110d21575525
alvarosc2 <alvarosc2@gmail.com>
2025-11-11 22:53:04 -0600

    high order array methods refactor

--- high-order-array-methods/high-order-run.ts
+++ high-order-array-methods/high-order-run.ts
@@ -1,6 +1,12 @@
 //@ts-check
 
-import { doubleNumbers, evenNumbers, oddNumbers, sum, mean } from './high-order-lib';
+import {
+    doubleNumbers,
+    evenNumbers,
+    oddNumbers,
+    sum,
+    mean
+} from './high-order-lib';
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
