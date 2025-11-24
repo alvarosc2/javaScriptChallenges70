d86fbee04df3ff3e83a0f3180d7beed41d7f831c
alvarosc2 <alvarosc2@gmail.com>
2025-11-24 00:18:05 -0600

    max subarray sum algorithm: starting solution

---
+++ maximum-subarray-sum/max-subarray-sum-run.ts
@@ -0,0 +1,8 @@
+//@ts-check
+
+import { maxSubarraySum } from './max-subarray-sum';
+
+const arr1 = [2, 5, 3, 1, 11, 7, 6, 4];
+const k1 = 3;
+
+console.log(maxSubarraySum(arr1, k1)); //expected output 24
---
+++ maximum-subarray-sum/max-subarray-sum.ts
@@ -0,0 +1,7 @@
+function maxSubarraySum(arr: number[], k: number): number {
+    let maxSum = 0;
+
+    return maxSum;
+}
+
+export { maxSubarraySum }
