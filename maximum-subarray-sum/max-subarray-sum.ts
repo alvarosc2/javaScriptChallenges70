a73a0e128a66298583d9eb60dbb44d6fded36867
alvarosc2 <alvarosc2@gmail.com>
2025-11-24 00:35:58 -0600

    A solution was achieved. Not elegant at all.

--- maximum-subarray-sum/max-subarray-sum.ts
+++ maximum-subarray-sum/max-subarray-sum.ts
@@ -1,5 +1,18 @@
 function maxSubarraySum(arr: number[], k: number): number {
     let maxSum = 0;
+    let tempVar = 0;
+
+    for (let i = 0; i <= arr.length - k; i++) {
+        for (let j = i; j < k + i; j++) {
+            tempVar += arr[j];
+        }
+
+        if (tempVar > maxSum) {
+            maxSum = tempVar;
+        }
+
+        tempVar = 0;
+    }
 
     return maxSum;
 }
