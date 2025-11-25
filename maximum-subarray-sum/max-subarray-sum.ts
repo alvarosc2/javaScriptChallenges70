ef36afc1293a6a56daa1bdc0e222468599b28403
alvarosc2 <alvarosc2@gmail.com>
2025-11-24 22:22:39 -0600

    Introducing sliding window technique to avoid O(n^2) time complexity.

--- maximum-subarray-sum/max-subarray-sum.ts
+++ maximum-subarray-sum/max-subarray-sum.ts
@@ -17,4 +17,21 @@ function maxSubarraySum(arr: number[], k: number): number {
     return maxSum;
 }
 
-export { maxSubarraySum }
+function maxSubarraySum2(arr: number[], k: number): number {
+    let currentSum: number = 0;
+    let maxSum: number = 0;
+
+    for (let i: number = 0; i < k; i++) {
+        maxSum += arr[i];
+    }
+
+    currentSum = maxSum;
+
+    for (let i = k; i < arr.length; i++) {
+        currentSum = currentSum - arr[i - k] + arr[i];
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+export { maxSubarraySum, maxSubarraySum2 }
