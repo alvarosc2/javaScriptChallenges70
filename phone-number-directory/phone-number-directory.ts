7e56494eb00d51891c0d9afa9a170d9bda7ccebb
alvarosc2 <alvarosc2@gmail.com>
2025-12-10 15:50:32 -0600

    refactoring phoneNumberDirectory function

--- phone-number-directory/phone-number-directory.ts
+++ phone-number-directory/phone-number-directory.ts
@@ -1,13 +1,9 @@
 function phoneNumberDirectory(phoneNumberList: string[]): Map<string, string> {
     let directory = new Map();
-    let splitedStrings = [];
 
     for (const item of phoneNumberList) {
-        splitedStrings.push(item.split(':'));
-    }
-
-    for (const item of splitedStrings) {
-        directory.set(item[0], item[1]);
+        const [name, phone] = item.split(':');
+        directory.set(name, phone);
     }
 
     return directory;
