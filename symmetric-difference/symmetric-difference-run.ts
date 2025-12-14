f63cb13b1360078ac823f1e1828be99029695f71
alvarosc2 <alvarosc2@gmail.com>
2025-12-14 12:58:24 -0600

    symmetric differences algorithm using sets

---
+++ symmetric-difference/symmetric-difference-run.ts
@@ -0,0 +1,9 @@
+//@ts-check
+
+import { symmetricDifference } from './symmetric-difference';
+
+const arr1 = [1, 2, 3];
+const arr2 = [3, 4, 5];
+
+const result = symmetricDifference(arr1, arr2);
+console.log(result);
