0a88834c56b0a18635b29bde515651f31e59043d
alvarosc2 <alvarosc2@gmail.com>
2025-12-25 21:17:00 -0600

    Two sum problem solved using sets

---
+++ two-sum-sets-version/two-sum-run.ts
@@ -0,0 +1,12 @@
+//@ts-check
+
+import { twoSum } from './two-sum';
+
+// Output: [0, 1] (2 + 7 = 9)
+console.log(twoSum([2, 7, 11, 15], 9));
+
+// Output: [1, 2] (2 + 4 = 6)
+console.log(twoSum([3, 2, 4], 6));
+
+// Output: [0, 1] (3 + 3 = 6)
+console.log(twoSum([3, 3], 6));
---
+++ two-sum-sets-version/two-sum.ts
@@ -0,0 +1,17 @@
+function twoSum(nums: number[], target: number): number[] {
+    const numSet = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (numSet.has(complement)) {
+            return [nums.indexOf(complement), i];
+        }
+
+        numSet.add(nums[i]);
+    }
+
+    return [];
+}
+
+export { twoSum };
