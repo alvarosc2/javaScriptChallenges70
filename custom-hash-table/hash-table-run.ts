b4d9d2b51176916a2c3fbddb37a112c8c9c413a6
alvarosc2 <alvarosc2@gmail.com>
2025-12-28 23:15:09 -0600

    Custom hash table implementation

--- custom-hash-table/hash-table-run.ts
+++ custom-hash-table/hash-table-run.ts
@@ -1,3 +1,12 @@
 //@ts-check
 
 import { HashTable } from './hash-table';
+
+const myHashTable = new HashTable;
+//const result = myHashTable._hash('john', myHashTable.limit);
+//console.log(result);
+
+myHashTable.set('john', '555-234-3455');
+myHashTable.set('Alvaro', '555-123-3455');
+
+myHashTable.printTable();
--- custom-hash-table/hash-table.ts
+++ custom-hash-table/hash-table.ts
@@ -1,3 +1,54 @@
-class HashTable { }
+class HashTable {
+    storage: Array<Array<[string, string]>>;
+    limit: number;
 
-module.exports = HashTable;
+    constructor(limit = 14) {
+        this.storage = [];
+        this.limit = limit;
+    }
+
+    _hash(key: string, max: number): number {
+        let hash = 0;
+
+        for (let i = 0; i < key.length; i++) {
+            hash += key.codePointAt(i);
+        }
+
+        return hash % max;
+    }
+
+    printTable() {
+        for (let i = 0; i < this.storage.length; i++) {
+            if (this.storage[i] !== undefined) {
+                console.log(`Bucket ${i}: ${JSON.stringify(this.storage[i])}`);
+            }
+            else {
+                console.log(`Bucket ${i} empty`);
+            }
+        }
+    }
+
+    set(key: string, value: string) {
+        const index = this._hash(key, this.limit);
+
+        if (this.storage[index] === undefined) {
+            this.storage[index] = [[key, value]];
+        }
+        else {
+            let inserted: boolean = false;
+
+            for (let i = 0; i < this.storage[index].length; i++) {
+                if (this.storage[index][i][0] === key) {
+                    this.storage[index][i][1] = value;
+                    inserted = true;
+                }
+            }
+
+            if (inserted === false) {
+                this.storage[index].push([key, value]);
+            }
+        }
+    }
+}
+
+export { HashTable };
